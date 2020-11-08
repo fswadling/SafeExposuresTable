153ca3ad8dd06be1d32733266693172dce45c719
Frederick Swadling <frederick.swadling@l-p-a.com>
2020-11-08 22:21:18 +0100

    Add bridge to app.fs

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -2,6 +2,7 @@ module App
 
 open Elmish
 open Elmish.React
+open Elmish.Bridge
 
 #if DEBUG
 open Elmish.Debug
@@ -9,10 +10,11 @@ open Elmish.HMR
 #endif
 
 Program.mkProgram Index.init Index.update Index.view
+|> Program.withBridgeConfig (Bridge.endpoint "./socket" |> Bridge.withUrlMode Append)
 #if DEBUG
 |> Program.withConsoleTrace
 #endif
-|> Program.withReactSynchronous "elmish-app"
+|> Program.withReactBatched "elmish-app"
 #if DEBUG
 |> Program.withDebugger
 #endif
