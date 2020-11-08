a366267fe24e19b2964c490c8436dde8bc4b0920
Frederick Swadling <frederick.swadling@l-p-a.com>
2020-11-08 15:31:34 +0100

    Added a database project

---
+++ src/Database/Context.cs
@@ -0,0 +1,19 @@
+using Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public class Context : DbContext
+    {
+        public DbSet<Exposure> Exposures { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Data Source=LPA-H-NB-182;Initial Catalog=SafeExposuresTable;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ src/Database/Entities/Exposure.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database.Entities
+{
+    public class Exposure
+    {
+        public DateTime ValueDate { get; set; }
+
+        public decimal Volume { get; set; }
+    }
+}
